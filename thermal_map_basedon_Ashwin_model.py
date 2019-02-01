bec955d30d4fc1b6c303fb5adc3127196676c803
Linyun He <sherryhly@gmail.com>
2019-02-01 14:13:02 -0600

    Similar to ipynb. Need to input two models and corresponding degrees.

--- thermal_map_basedon_Ashwin_model.py
+++ thermal_map_basedon_Ashwin_model.py
@@ -9,18 +9,28 @@ from PIL import Image
 import numpy as np
 import re
 from scipy import interpolate
+import matplotlib.pyplot as plt
 
 folder = input("What is your folder path:")
 albedo_filename = input("What is the filename of albedo:")
 TI_filename = input("What is the filename of thermal inertia:")
-thermal_model_filename = input("What is the filename of thermal model:")
+thermal_model_filename1 = input("What is the filename of thermal model 1:")
+degree1 = input("What is the degree of thermal model 1:")
+thermal_model_filename2 = input("What is the filename of thermal model 2:")
+degree2 = input("What is the degree of thermal model 2:")
+degree = input("What is your expected degree:")
 T_savefilename = input("Save name of output temperature:")
 
 albedo_file = folder+'/'+albedo_filename+'.hdr'
 band_num = 400
 TI_file = folder+'/'+TI_filename+'.tiff'
-thermal_model_file = folder+'/'+thermal_model_filename+'.txt'
+thermal_model_file1 = folder+'/'+thermal_model_filename1+'.txt'
+thermal_model_file2 = folder+'/'+thermal_model_filename2+'.txt'
 T_savefile = folder+'/'+T_savefilename
+degree1 = float(degree1)
+degree2 = float(degree2)
+degree = float(degree)
+
 
 
 # Read albedo and TI
@@ -49,16 +59,25 @@ print("Albedo start long: "+str(lon_start)+" and delta: "+str(lon_del))
 print("Albedo start lat: "+str(lat_start)+" and delta: "+str(lat_del))
 
 # Read THERMAL_MODEL
-model = open(thermal_model_file)
+model1 = open(thermal_model_file1)
+model2 = open(thermal_model_file2)
 start_flag = False
-model_table = list()
-for line in model:
+model_table1 = list()
+for line in model1:
     if start_flag:
         tmp_albedo, tmp_ti,tmp_t = re.findall("\d+\.\d+", line)
-        model_table.append([float(tmp_albedo),float(tmp_ti),float(tmp_t)])
+        model_table1.append([float(tmp_albedo),float(tmp_ti),float(tmp_t)])
     elif "Columns are Albedo" in line:
         start_flag = True
-
+start_flag = False
+model_table2 = list()
+for line in model2:
+    if start_flag:
+        tmp_albedo, tmp_ti,tmp_t = re.findall("\d+\.\d+", line)
+        model_table2.append([float(tmp_albedo),float(tmp_ti),float(tmp_t)])
+    elif "Columns are Albedo" in line:
+        start_flag = True
+        
 # Map TI to albedo domain
 OFFSET = 8145529
 print('Mapping TI to albedo domain...')
@@ -74,13 +93,16 @@ for i in range(num_row):
                 tmp_lon = lon_start+j*lon_del+OFFSET
                 if(tmp_lon>= TI_lon_start and tmp_lon <= TI_lon_end):
                     mapped_TI[i,j] = TI[round((TI_lat_start-tmp_lat)/TI_lat_del),round((tmp_lon-TI_lon_start)/TI_lon_del)]
-print('Mapping TI to albedo domain...done!')                    
-# Interpolate thermal model                    
-model_table = np.array(model_table)
-albedo_axis = model_table[:,0]
-TI_axis = model_table[:,1]
-T_axis = model_table[:,2]
+print('Mapping TI to albedo domain...done!')           
+
+# Interpolate thermal model
+model_table1 = np.array(model_table1)
+model_table2 = np.array(model_table2)
+albedo_axis = model_table1[:,0]
+TI_axis = model_table1[:,1]
+T_axis = (model_table2[:,2] - model_table1[:,2])*(degree-degree1)/(degree2-degree1)+model_table1[:,2]
 f = interpolate.interp2d(albedo_axis, TI_axis, T_axis, kind='cubic')
+
 # For all pixels
 print('Retrieving temperature map...')
 T_res = np.zeros((num_row,num_col))
@@ -90,5 +112,5 @@ for i in range(num_row):
             T_res[i,j] = f(albedo[i,j],mapped_TI[i,j])
 print('Retrieving temperature map...done!')            
 plt.imshow(T_res)
-spy.envi.save_image(T_savefile+'.hdr',T_res,force = True)
+spy.envi.save_image(T_savefile+'_'+str(degree)+'degree.hdr',T_res,force = True)
 
